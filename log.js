f6895afdc892d978eb95bc305cdebf8c2e1205bf
Dale Harvey <dale@arandomurl.com>
2010-10-05 09:31:22 +0100

    minor changes

--- log.js
+++ log.js
@@ -1,32 +1,47 @@
-var request = require('request')
-  , sys = require('sys')
-  , irc = require('./IRC/lib/irc')
-  ;
+var request = require('request'),
+    sys = require('sys'),
+    irc = require('./IRC/lib/irc');
   
-var ircOptions = { server: 'irc.freenode.net'};
-var couchOptions = { uri:'http://mikeal.couchone.com/couchdbirc'
-                   , method:'POST'
-                   , headers:{'content-type':'application/json'}
-                   }
-                   ;
+var ircOptions = {
+    server: 'irc.freenode.net'
+};
+
+var couchOptions = {
+    uri:'http://127.0.0.1:5984/couchdbirc',
+    method:'POST',
+    headers:{'content-type':'application/json'}
+};
+
+var opts = {
+    nick : 'couchlogbot',
+    room : '#couchlog',
+    roomPass : '******'
+};
 
 var bot = new irc(ircOptions);
 bot.connect(function () {
-  bot.nick('couchlog');
-  bot.join('#couchdb');
+  
+  bot.nick(opts.nick);
+
+  if (opts.roomPass) { 
+    bot.join(opts.room, opts.roomPass);
+  } else {
+    bot.join(opts.room);
+  }
+  
   bot.addListener('privmsg', function (message) {
     if (message.params.length === 2) {
       message.channel = message.params[0];
       message.message = message.params[1];
-      delete message.params
+      delete message.params;
     }
-    if (message.channel === '#couchdb') {
+    if (message.channel === opts.room) {
       message.timestamp = new Date();
       couchOptions.body = JSON.stringify(message);
       couchOptions.headers['content-length'] = couchOptions.body.length;
       request(couchOptions, function (err, resp, body) {
         sys.puts(body);
-      })
+      });
     }
-  })
+  });
 });
