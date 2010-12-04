d51180266fc896f855ad5972b6c601f25058a047
Jason Smith <jhs@couchone.com>
2010-12-04 21:43:33 +0700

    Also index the nickname of the person who spoke the tag

--- couchapp/views/tags-by-timestamp/map.js
+++ couchapp/views/tags-by-timestamp/map.js
@@ -2,11 +2,12 @@ function(doc) {
   var tag_re = /\B#(\w|[_-])+/g; // Hash then anything, as long as the hash isn't e.g. in a URL.
 
   if(doc.message && doc.timestamp) {
-    //var stamp = doc.timestamp.split('-');
-    //stamp[2] = stamp[2].substr(0, 2);
     var stamp = doc.timestamp;
-
     var matches = doc.message.match(tag_re) || [];
+
+    if(matches.length)
+      emit([doc.person.nick, stamp], null);
+
     matches.forEach(function(tag) {
       // Emit once for "#tag" and once for "tag" to be more human-friendly.
       emit([tag                  , stamp], null);
